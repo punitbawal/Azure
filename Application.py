936866438d528eed78c5b62f1c3d4bd58c52e0e6
punitbawal <punitbawal@gmail.com>
2019-02-22 19:42:55 -0600

    Added DB Engine Included SQLAlchemy, Pandas & pyodbc requirement

--- Application.py
+++ Application.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import pandas as pd
+from sqlalchemy import create_engine, types
+import urllib
 
 app = Flask(__name__)
+params = urllib.parse.quote_plus("Driver={ODBC Driver 13 for SQL Server};Server=tcp:punitdb.database.windows.net,1433;Database=punitdb;Uid=punitbawal@punitdb;Pwd={BAW123pun};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
 
 @app.route('/')
@@ -9,4 +14,6 @@ def hello_world():
 
 @app.route('/hi')
 def hi_world():
-    return 'Hi World. Again!'
+    cnt = engine.execute("SELECT count(*) FROM earthquake").fetchall()
+    print(cnt[0][0])
+    return 'Hi World. Again!'+str(cnt[0][0])
